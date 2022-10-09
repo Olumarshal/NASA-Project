435df5a80f17f3154825e84e38061df57a705a61
Marshal Olu <olmarsh.192@gmail.com>
2022-10-09 13:07:02 +0100

    added the planets model

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const { 
+    getAllPlanets,
+ } = require("./planets.controllers");
+
 const planetsRouter = express.Router();
 
 planetsRouter.get("/planet", getAllPlanets);
 
-module.exports = planetsRouter
+module.exports = planetsRouter;
